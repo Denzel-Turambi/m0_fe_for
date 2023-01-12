44e5deeba334063fdae5799add68b5282093605a
Denzel Turambi <dturambi26@gmail.com>
2023-01-11 23:00:11 -0700

    Add final challenge

--- for.js
+++ for.js
@@ -11,11 +11,18 @@ for (var i = 26; i <75; i += 2) {
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
-
+for (var i = 0; i <= 4; i++) {
+  console.log("The weather today will be " + weather_options[i])
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+for (var i = 0; i <= 6;i++) {
+  if (words[i].length === 3) {
+    console.log(words[i])
+  }
+}
 
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
